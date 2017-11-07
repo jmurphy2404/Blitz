aad4ea2335b9dada1220e7c4361822d5103cdd72
Lisa Mae <lisamae135b7@me.com>
2017-11-07 11:43:24 -0500

    made simple nav bar and implemented boostrap gems and home page folder with controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/' =>'home#index', as: :root
   resources :jobs
   resources :bikes
   resources :users
 
-  root "jobs#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

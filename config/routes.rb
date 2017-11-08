5359f382e987b101a88e3002f01e7aa238f6b02d
Lisa Mae <lisamae135b7@me.com>
2017-11-07 20:36:35 -0500

    fixed nav routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :jobs
   resources :bikes
   resources :users
+
   get 'logout' => "home#logout"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
